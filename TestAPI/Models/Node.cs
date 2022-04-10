df7eb6eb029e14aa28538eab58a66707cd81304a
Álvaro <lvaro@DESKTOP-FOPBR3K>
2022-04-10 14:46:17 +0200

    Add project files.

---
+++ TestAPI/Models/Node.cs
@@ -0,0 +1,20 @@
+﻿namespace TestAPI.Models
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public string? longId { get; set; }
+        public int DatacenterId { get; set; } // ID del Centro de Datos. Ligado a Models.Datacenter
+
+        public string? CPU { get; set; } // Nombre completo del procesador (Marca + Modelo)
+        public int CPUCores { get; set; } // Número de núcleos del CPU
+        public string? RAM { get; set; } // Memoria RAM total del Nodo en MiB
+
+        public double Cost { get; set; } // Coste mensual en €
+
+        public bool Pterodactyl { get; set; } // Wether or not el nodo está conectado a Pterodactyl
+        public bool Monolith { get; set; } // Wether or not es un monolito
+        public bool Docker { get; set; } // Wether or not usa Docker
+        public bool Kubernetes { get; set; } // Wether or not usa Kubernetes
+    }
+}
