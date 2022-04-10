dba3a90d8d3100866431ff7bf5f535ed456aaeb6
Paquito86 <ads847ads@gmail.com>
2022-04-10 19:06:14 +0200

    Deleted WeatherForecast default endpoint

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -23,25 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapGet("/", () => "¡Hello! -- IRIS by AlphaTech API v1");
 
 #region Node Endpoints
@@ -95,8 +76,3 @@ app.MapDelete("/node/{id}", async (NodeDb db, int id) =>
 #endregion
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
