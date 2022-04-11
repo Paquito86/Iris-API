3fd598d7567b3aeba5144c2f5f25e2a5872afc41
Paquito86 <ads847ads@gmail.com>
2022-04-11 15:45:57 +0200

    Created SSHKeyAgent Class for generating SSH-RSA Keys

---
+++ TestAPI/Agents/SSHKeyAgent.cs
@@ -0,0 +1,19 @@
+﻿namespace TestAPI.Agents
+{
+    public class SSHKeyAgent
+    {
+        public void SSHGen()
+        {
+            var keygen = new SshKeyGenerator.SshKeyGenerator(2048);
+
+            var privateKey = keygen.ToPrivateKey();
+            Console.WriteLine(privateKey);
+
+            var publicSshKey = keygen.ToRfcPublicKey();
+            Console.WriteLine(publicSshKey);
+
+            var publicSshKeyWithComment = keygen.ToRfcPublicKey("user@domain.com");
+            Console.WriteLine(publicSshKeyWithComment);
+        }
+    }
+}
